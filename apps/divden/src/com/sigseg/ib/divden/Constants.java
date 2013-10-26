4bf987c7fdb01e01e01c034b06a26bbc5ed1021e
John Lombardo <john@lombardos.org>
2013-10-26 16:55:51 -0700

    Getting the price now works

---
+++ apps/divden/src/com/sigseg/ib/divden/Constants.java
@@ -0,0 +1,13 @@
+package com.sigseg.ib.divden;
+
+public interface Constants {
+    /**
+     * These vaues are observed when running the TestJavaClient
+     */
+    public interface JavaClient {
+        /**
+         * This comes from OrderDlg
+         */
+        public final String GENERIC_TICKS = "100,101,104,105,106,107,165,221,225,233,236,258,293,294,295,318";
+    }
+}
--- apps/divden/src/com/sigseg/ib/divden/Divden.java
+++ apps/divden/src/com/sigseg/ib/divden/Divden.java
@@ -2,11 +2,14 @@ package com.sigseg.ib.divden;
 
 import com.ib.client.*;
 
+import java.util.Vector;
+
 /**
  * Divden implements the divden approach
  */
-public class Divden implements EWrapper {
+public class Divden implements EWrapper,Constants {
 
+    /* Constants */
     private final static double CASH_WIN = 10.00;
     private final static int NUM_SHARES = 1;
     private final static double RISK_CURRENCY = 10000.00;
@@ -14,8 +17,7 @@ public class Divden implements EWrapper {
     private final static double TRANSACTION_COST = 1.50;
     private final static int TWS_PORT = 7496;
 
-    public class DivdenException extends Exception{public DivdenException(String m){super(m);}}
-
+    /* Input parameters */
     private String account = null;
     private double cashWin = CASH_WIN;
     private int numShares = NUM_SHARES;
@@ -25,9 +27,19 @@ public class Divden implements EWrapper {
     private double transactionCost = TRANSACTION_COST;
     private int twsPort = TWS_PORT;
 
-    private EClientSocket ib = new EClientSocket(this);
-
+    private EClientSocket ibServer = new EClientSocket(this);
     private final Logger log;
+
+    static int tradeId = 0;
+    private class Trade {
+        int id;
+        Contract contract;
+        public Trade(){
+            this.id = ++tradeId;
+        }
+    }
+
+    public class DivdenException extends Exception{public DivdenException(String m){super(m);}}
 
     public Divden(Logger logger){
         this.log = logger;
@@ -42,9 +54,47 @@ public class Divden implements EWrapper {
     public void start() {
         report();
 
-        ib.eConnect("localhost", twsPort, 0);
-        if (ib.isConnected()){
-            ib.reqAccountSummary(1, "All", "BuyingPower");
+        ibServer.eConnect("localhost", twsPort, 0);
+        if (ibServer.isConnected()){
+            countAndDisconnect(true);
+            ibServer.reqAccountSummary(1, "All", "BuyingPower");
+
+            Trade trade = new Trade();
+            trade.contract = new Contract(
+                0,                      // int p_conId
+                symbol,                 // String p_symbol
+                "STK",                  // String p_secType
+                "",                     // String p_expiry
+                0.0,                    // double p_strike
+                "",                     // String p_right
+                "",                     // String p_multiplier
+                "SMART",                // String p_exchange
+                "USD",                  // String p_currency
+                "",                     // String p_localSymbol
+                "",                     // String p_tradingClass
+                new Vector<ComboLeg>(), // Vector<ComboLeg> p_comboLegs
+                "",                     // String p_primaryExch
+                false,                  // boolean p_includeExpired
+                "",                     // String p_secIdType
+                ""                      // String p_secId
+            );
+
+            countAndDisconnect(true);
+            ibServer.reqMktData(trade.id, trade.contract, JavaClient.GENERIC_TICKS, false);
+        }
+    }
+
+    int count=0;
+    private void countAndDisconnect(boolean isIncrement){
+        synchronized (this){
+            if (isIncrement)
+                count++ ;
+            else {
+                count--;
+                if (count==0){
+                    ibServer.eDisconnect();
+                }
+            }
         }
     }
 
@@ -64,7 +114,7 @@ public class Divden implements EWrapper {
                 reqId, account, tag, value, currency);
     }
     @Override public void accountSummaryEnd(int reqId) {
-        ib.eDisconnect();
+        countAndDisconnect(false);
     }
     @Override public void bondContractDetails(int reqId, ContractDetails contractDetails) { }
     @Override public void commissionReport(CommissionReport commissionReport) { }
@@ -73,9 +123,15 @@ public class Divden implements EWrapper {
     @Override public void contractDetailsEnd(int reqId) { }
     @Override public void currentTime(long time) { }
     @Override public void deltaNeutralValidation(int reqId, UnderComp underComp) { }
-    @Override public void error(Exception e) { }
-    @Override public void error(String str) { }
-    @Override public void error(int id, int errorCode, String errorMsg) { }
+    @Override public void error(Exception e) {
+        log.err("Exception: %s",e.getMessage());
+    }
+    @Override public void error(String str) {
+        log.err(str);
+    }
+    @Override public void error(int id, int errorCode, String errorMsg) {
+        log.err("id=%d code=%d msg=%s",id,errorCode,errorMsg);
+    }
     @Override public void execDetails(int reqId, Contract contract, Execution execution) { }
     @Override public void execDetailsEnd(int reqId) { }
     @Override public void fundamentalData(int reqId, String data) { }
@@ -96,9 +152,14 @@ public class Divden implements EWrapper {
     @Override public void tickEFP(int tickerId, int tickType, double basisPoints, String formattedBasisPoints, double impliedFuture, int holdDays, String futureExpiry, double dividendImpact, double dividendsToExpiry) { }
     @Override public void tickGeneric(int tickerId, int tickType, double value) { }
     @Override public void tickOptionComputation(int tickerId, int field, double impliedVol, double delta, double optPrice, double pvDividend, double gamma, double vega, double theta, double undPrice) { }
-    @Override public void tickPrice(int tickerId, int field, double price, int canAutoExecute) { }
+    @Override public void tickPrice(int tickerId, int field, double price, int canAutoExecute) {
+        log.out("tickerId=%d field=%d price=%f canAutoExecute=%b",
+                tickerId,field,price,canAutoExecute);
+    }
     @Override public void tickSize(int tickerId, int field, int size) { }
-    @Override public void tickSnapshotEnd(int reqId) { }
+    @Override public void tickSnapshotEnd(int reqId) {
+        countAndDisconnect(false);
+    }
     @Override public void tickString(int tickerId, int tickType, String value) { }
     @Override public void updateAccountTime(String timeStamp) { }
     @Override public void updateAccountValue(String key, String value, String currency, String accountName) { }
