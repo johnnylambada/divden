9d7068ad418d0cffab4417f22400d8a18eeba8cc
John Lombardo <john@lombardos.org>
2014-09-26 15:28:58 -0700

    Updated to latest tws

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -53,7 +53,7 @@ function tws
 {
   (
   cd $(gettop)/external/tws/IBJts
-  java -cp jts.jar:hsqldb.jar:jcommon-1.0.12.jar:jfreechart-1.0.9.jar:jhall.jar:other.jar:rss.jar \
+  java -cp jts.jar:total.2013.jar \
        -Xmx512M \
        -XX:MaxPermSize=128M \
        jclient.LoginFrame .
